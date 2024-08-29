01be30850e732716d55525f8ab1419ae7bb573c9
BalajiChavan1306 <balajichavan1306@gmail.com>
2024-08-29 17:27:18 +0530

    Iintial commit of JavaScript exercise

---
+++ Array/Array.js
@@ -0,0 +1,9 @@
+
+let colors = ["red", "green", "blue"];
+console.log(colors[0]); 
+console.log(colors[1]); 
+colors.push("yellow");
+console.log(colors); 
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
